8b63241bdebefa8b6b329319b04bb65ab58948de
ameriyana <eriyanutt354@gmail.com>
2023-01-27 09:47:29 +0700

    test

---
+++ main.py
@@ -0,0 +1,9 @@
+
+if __name__ == '__main__':
+    n = int(input("angka").strip())
+    if n < 4 :
+        print("Weird")
+    elif n >= 4 :
+        print("Not Weird")
+    elif n == 5 :
+        print("Weird")
